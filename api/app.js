e1e2ccdc9bbc63b9344386bc45f2503588bf48e3
Paramveer Singh <paramveerpuru@gmail.com>
2024-06-05 23:07:42 +0530

    initial commit

---
+++ api/app.js
@@ -0,0 +1,43 @@
+import dotenv from "dotenv"
+import express from "express"
+import mongoose from "mongoose"
+// import bodyParser from "body-parser"
+import {UserModel as User} from "./models/User.js"
+import jwt from "jsonwebtoken"
+import cors from "cors"
+
+dotenv.config()
+const PORT = 3000
+const app = express()
+
+
+// console.log(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL)
+//add error handling for this
+
+
+app.use(express.json())
+app.use(cors({
+	credentials:true,
+	origin: process.env.CLIENT_URL
+}))
+
+app.get('/', (req, res) => {
+	res.send("test ok")
+})
+
+app.post('/register', async (req, res) => {
+	try{
+		const createdUser = await User.create(req.body)
+		const token = jwt.sign({userId: createdUser._id}, process.env.JWT_SECRET)
+		res.cookie('token', token).status(201).json({message: 'User created successfully!'})
+	}catch(error){
+		res.json({message: error.message || "An error occurred while creating the user"})
+	}
+	// res.send({message: "hello"})
+})
+
+app.listen(PORT, error => {
+	console.log(`Server running on port ${PORT}.`)
+})
+
---
+++ api/models/User.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose"
+
+const UserSchema = new mongoose.Schema({
+	username: {type: String, unique: true},
+	password: {type: String, unique: true},
+}, {timestamps: true})
+
+export const UserModel = mongoose.model('User', UserSchema)
